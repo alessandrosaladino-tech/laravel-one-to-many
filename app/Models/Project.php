2f9593f5c152c6287da97e89b4d5944fb6b9c30c
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-11-13 16:33:59 +0100

    Create model and migration for type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,4 +13,14 @@ class Project extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['title', 'slug', 'thumb', 'description', 'type', 'github_link', 'public_link', 'release_date'];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public static function generateSlug($string)
+    {
+        return Str::slug($string, '-');
+    }
 }
